d98b9922645e732cf079f7209d1e1ffff2275c8b
rajeshwaranx-dev <115334651+rajeshwaranx-dev@users.noreply.github.com>
2026-02-27 14:36:53 +0530

    Update users_chats_db.py

--- plugins/users_chats_db.py
+++ plugins/users_chats_db.py
@@ -7,7 +7,7 @@ client = AsyncIOMotorClient(DATABASE_URI)
 mydb = client[DATABASE_NAME]
 
 class Database:
-   default = {
+    default = {
         'spell_check': SPELL_CHECK,
         'auto_filter': AUTO_FILTER,
         'file_secure': PROTECT_CONTENT,
@@ -22,13 +22,13 @@ class Database:
         'api_two': SHORTENER_API2,
         'log': LOG_VR_CHANNEL,
         'imdb': IMDB,
-        'link': LINK_MODE, 
-        'is_verify': IS_VERIFY, 
+        'link': LINK_MODE,
+        'is_verify': IS_VERIFY,
         'verify_time': 43200,
         'fsub_id': AUTH_CHANNEL,
         'fsub_link': FSUB_LINK,
-}
-    
+    }
+
     def __init__(self):
         self.col = mydb.users
         self.grp = mydb.groups
@@ -55,7 +55,7 @@ class Database:
 
     async def find_join_req(self, id):
         return bool(await self.req.find_one({'id': id}))
-        
+
     async def add_join_req(self, id):
         await self.req.insert_one({'id': id})
 
@@ -74,19 +74,19 @@ class Database:
                 reason=""
             )
         )
-    
+
     async def add_user(self, id, name):
         user = self.new_user(id, name)
         await self.col.insert_one(user)
-    
+
     async def is_user_exist(self, id):
         user = await self.col.find_one({'id':int(id)})
         return bool(user)
-    
+
     async def total_users_count(self):
         count = await self.col.count_documents({})
         return count
-    
+
     async def get_all_users(self):
         return self.col.find({})
 
@@ -95,34 +95,34 @@ class Database:
 
     async def delete_chat(self, id):
         await self.grp.delete_many({'id': int(id)})
-        
+
     async def get_banned(self):
         users = self.col.find({'ban_status.is_banned': True})
         chats = self.grp.find({'chat_status.is_disabled': True})
         b_chats = [chat['id'] async for chat in chats]
         b_users = [user['id'] async for user in users]
         return b_users, b_chats
-    
+
     async def add_chat(self, chat, title):
         chat = self.new_group(chat, title)
         await self.grp.insert_one(chat)
 
     async def get_chat(self, chat):
         chat = await self.grp.find_one({'id':int(chat)})
-        return False if not chat else chat.get('chat_status')  
+        return False if not chat else chat.get('chat_status')
 
     async def update_settings(self, id, settings):
-        await self.grp.update_one({'id': int(id)}, {'$set': {'settings': settings}})   
-    
+        await self.grp.update_one({'id': int(id)}, {'$set': {'settings': settings}})
+
     async def total_chat_count(self):
         count = await self.grp.count_documents({})
         return count
-    
+
     async def get_all_chats(self):
         return self.grp.find({})
 
     async def get_db_size(self):
-        return (await mydb.command("dbstats"))['dataSize'] 
+        return (await mydb.command("dbstats"))['dataSize']
 
     async def get_notcopy_user(self, user_id):
         user_id = int(user_id)
@@ -153,10 +153,9 @@ class Database:
         ist_timezone = pytz.timezone('Asia/Kolkata')
         pastDate = pastDate.astimezone(ist_timezone)
         current_time = datetime.datetime.now(tz=ist_timezone)
-        seconds_since_midnight = (current_time - datetime.datetime(current_time.year, current_time.month, current_time.day, 0, 0, 0, tzinfo=ist_timezone)).total_seconds()
         time_diff = current_time - pastDate
         total_seconds = time_diff.total_seconds()
-        return total_seconds <= seconds_since_midnight
+        return total_seconds <= 43200
 
     async def user_verified(self, user_id):
         user = await self.get_notcopy_user(user_id)
@@ -168,10 +167,9 @@ class Database:
         ist_timezone = pytz.timezone('Asia/Kolkata')
         pastDate = pastDate.astimezone(ist_timezone)
         current_time = datetime.datetime.now(tz=ist_timezone)
-        seconds_since_midnight = (current_time - datetime.datetime(current_time.year, current_time.month, current_time.day, 0, 0, 0, tzinfo=ist_timezone)).total_seconds()
         time_diff = current_time - pastDate
         total_seconds = time_diff.total_seconds()
-        return total_seconds <= seconds_since_midnight
+        return total_seconds <= 43200
 
     async def use_second_shortener(self, user_id, time):
         user = await self.get_notcopy_user(user_id)
@@ -195,28 +193,6 @@ class Database:
                 return second_time < pastDate
         return False
 
-    async def use_third_shortener(self, user_id, time):
-        user = await self.get_notcopy_user(user_id)
-        if not user.get("third_time_verified"):
-            ist_timezone = pytz.timezone('Asia/Kolkata')
-            await self.update_notcopy_user(user_id, {"third_time_verified":datetime.datetime(2018, 5, 17, 0, 0, 0, tzinfo=ist_timezone)})
-            user = await self.get_notcopy_user(user_id)
-        if await self.user_verified(user_id):
-            try:
-                pastDate = user["second_time_verified"]
-            except Exception:
-                user = await self.get_notcopy_user(user_id)
-                pastDate = user["second_time_verified"]
-            ist_timezone = pytz.timezone('Asia/Kolkata')
-            pastDate = pastDate.astimezone(ist_timezone)
-            current_time = datetime.datetime.now(tz=ist_timezone)
-            time_difference = current_time - pastDate
-            if time_difference > datetime.timedelta(seconds=time):
-                pastDate = user["second_time_verified"].astimezone(ist_timezone)
-                second_time = user["third_time_verified"].astimezone(ist_timezone)
-                return second_time < pastDate
-        return False
-   
     async def create_verify_id(self, user_id: int, hash):
         res = {"user_id": user_id, "hash":hash, "verified":False}
         return await self.verify_id.insert_one(res)
@@ -232,7 +208,7 @@ class Database:
     async def get_user(self, user_id):
         user_data = await self.users.find_one({"id": user_id})
         return user_data
-        
+
     async def update_user(self, user_data):
         await self.users.update_one({"id": user_data["id"]}, {"$set": user_data}, upsert=True)
 
@@ -247,7 +223,7 @@ class Database:
             else:
                 await self.users.update_one({"id": user_id}, {"$set": {"expiry_time": None}})
         return False
-        
+
     async def update_one(self, filter_query, update_data):
         try:
             result = await self.users.update_one(filter_query, update_data)
