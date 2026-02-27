8a81b6b98e609129a74bb64c7c16ec61447d4ed3
rajeshwaranx-dev <115334651+rajeshwaranx-dev@users.noreply.github.com>
2026-02-27 10:03:51 +0530

    Update info.py

--- info.py
+++ info.py
@@ -58,7 +58,7 @@ LANGUAGES = ["hindi", "english", "telugu", "tamil", "kannada", "malayalam"]
 
 auth_channel = environ.get('AUTH_CHANNEL', '-1002853931793')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
-FSUB_LINK = os.environ.get("FSUB_LINK", "0") 
+FSUB_LINK = os.environ.get("FSUB_LINK", "https://t.me/THANOS_LINKZ") 
 SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', '-1002844979596'))
 
 # bot settings
